b4c5dacbc97c1afcc731d9b06a5c47910363056a
Roman Hossain Shaon <roman.shaon@gmail.com>
2021-03-24 15:40:55 +0600

    try function uses

--- connect.php
+++ connect.php
@@ -6,3 +6,17 @@ if($db){
     echo"not connect";
 };
 ?>
+
+<!-- <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=my_dbms", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?> -->
